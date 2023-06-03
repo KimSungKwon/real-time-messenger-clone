1e5499337651724bdfe453e08fba834dc99dca5e
KimSungKwon <sungkwonn@naver.com>
2023-06-03 15:44:05 +0900

    set react-hot-toast context

---
+++ app/context/ToasterContext.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+// app/providers/ToasterProvider와 같음
+import { Toaster } from "react-hot-toast";
+
+const ToasterContext = () => {
+  return (  
+    <Toaster />
+  );
+}
+ 
+export default ToasterContext;
